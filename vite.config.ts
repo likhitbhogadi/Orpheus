dfba082b3965a2107e408f8cd6bc2c4bdc750529
Likhit Bhogadi <likhitbhogadi@gmail.com>
2025-02-26 17:13:26 +0530

    a2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Orpheus/',  // Add this line
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
